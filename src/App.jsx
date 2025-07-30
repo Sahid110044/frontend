37a36d6efe4319a854ba279a9b962d702fe09e84
sahid110044 <sahid110044@gmail.com>
2025-07-30 10:17:15 +0530

    added some text

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <>
      <h1>hello shahid</h1>
+     <h2>well i am glad to announce this</h2>
+     <h2>i have recently complited my first vercel mern deploy</h2>
     </>
   )
 }
